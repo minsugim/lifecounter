e029d8039ca49c5a3c453202c33b50765d60b725
Minsu Gim <minsugim@uw.edu>
2018-04-19 19:41:46 -0700

    Updated files

--- app/src/main/java/edu/uw/ischool/minsugim/lifecounter/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/minsugim/lifecounter/MainActivity.kt
@@ -2,11 +2,44 @@ package edu.uw.ischool.minsugim.lifecounter
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.GridLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myRoot = findViewById<GridLayout>(R.id.my_root)
+        val player1 = findViewById<TextView>(R.id.player1_lives)
+
+
+//            player1.findViewWithTag<Button>("button").setOnClickListener {
+//                var lives = player1.text.toString().toInt()
+//                lives++
+//                player1.setText(lives.toString())
+//            }
+
     }
 }
+
+//class Player(val player: String = "") {
+//    var lives: Int = 20
+//
+//    inner class PlusButton : Button {
+//        constructor(ctx: Context, attrs: AttributeSet) : super(ctx, attrs)
+//
+//        override fun setOnClickListener(l: OnClickListener?) {
+//            lives++
+//        }
+//    }
+//
+//    inner class minusButton : Button {
+//        constructor(ctx: Context, attrs: AttributeSet) : super(ctx, attrs)
+//
+//        override fun setOnClickListener(l: OnClickListener?) {
+//            lives--
+//        }
+//    }
+//}
